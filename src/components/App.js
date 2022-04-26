df878745bfbf24832da91b4afcb478864c3ef479
Aimarest <amarcosestevez@gmail.com>
2022-04-26 09:15:28 +0200

    form

--- src/components/App.js
+++ src/components/App.js
@@ -4,9 +4,32 @@ import phrasesList from "../data/phrases.json";
 function App() {
   //Variables de estado:
   const [data, setData] = useState(phrasesList);
+  const htmlData = data.map((phrase, index) => <li></li>);
   return (
     <div className="App">
-      <h1>Hola mundo</h1>
+      <header>
+        <h1 className="header__title">Frases de Friends</h1>
+        <form>
+          <label htmlFor="search">Filtrar por frase</label>
+          <input
+            className="header__search"
+            autoComplete="off"
+            type="search"
+            name="search"
+            placeholder="Filtrar frases"
+          />
+          <label htmlFor="character">Filtrar por personaje</label>
+          <select>
+            <option value="Todos">Todos</option>
+            <option value="Joey">Joey</option>
+            <option value="Rachel">Rachel</option>
+            <option value="Chandler">Chandler</option>
+            <option value="Phoebe">Phoebe</option>
+            <option value="Ross">Ross</option>
+            <option value="Monica">Monica</option>
+          </select>
+        </form>
+      </header>
     </div>
   );
 }
